59d0c647718a0c75c07590f8bf6ac008a9e00952
Sanglap <putatundasanglap@gmail.com>
2020-05-31 20:16:45 +0530

    Update Sara.py

--- Sara.py
+++ Sara.py
@@ -129,8 +129,8 @@ def grabPhoto():
 # User Details.
 name = 'Sanglap'
 bot = 'sara'
-emailadd = 'ahardlyunknown@gmail.com'
-pword = 'Hardly_1234'
+emailadd = '******************'
+pword = '**********************'
 
 os.system('cls')
 print(green + "\n\t<!!! ONLINE !!!>" + reset)
